7d576a87c85b322a50f9c37c29be31beed839bd3
ravinda anjana <ravinda.anjana50@gmail.com>
2022-10-18 11:50:48 +0530

    Added Functions & Enums

---
+++ DatabaseWebAPI/Other/Enums.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DatabaseWebAPI.Other
+{
+    public class Enums
+    {
+        public enum StatusEnums
+        {
+            [Display(Name = "Active")]
+            Active = 1,
+            [Display(Name = "Deactive")]
+            Deactive = 2,
+            [Display(Name = "Pending")]
+            Pending = 3,
+            [Display(Name = "Completed")]
+            Completed = 4,
+            [Display(Name = "Removed")]
+            Removed = 5,
+            [Display(Name = "Passed")]
+            Passed = 6,
+            [Display(Name = "Failed")]
+            Failed = 7,
+        }
+    }
+}
